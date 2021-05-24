916b15a17d3d30976f878c73f8bb2ecf8be96979
Łukasz Marciszewski <lmarciszewski1@gmail.com>
2021-05-24 21:35:13 +0200

    move check

--- src/components/ActiveVideo/ActiveVideo.js
+++ src/components/ActiveVideo/ActiveVideo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import 'plyr/dist/plyr.css'
+// import 'plyr/dist/plyr.css'
 import ReactPlayer from 'react-player'
 const useStyles = makeStyles((theme) => ({
     container: {
