735e563b4203e76c703200208d66e73c80ada46a
Łukasz Marciszewski <lmarciszewski@gravityglobal.com>
2022-10-17 11:00:20 +0200

    remove backend

--- config.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-module.exports = {
-  PORT: process.env.PORT,
-  CONNECTION_URL: process.env.CONNECTION_URL,
-}
--- routes/apiVideos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const videos = require('../controllers/videos')
-
-router.get('/videos', videos.getVideos)
-
-router.get('/videos/:id', videos.getVideo)
-
-module.exports = router
